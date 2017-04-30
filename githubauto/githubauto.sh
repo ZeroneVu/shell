86a2403f9d97401e68dc02f2358108c276a99ea4
Zerone Vu <zeronevu@gmail.com>
2017-05-01 00:07:09 +0700

    update githunauto.sh to correct code

--- githubauto/githubauto.sh
+++ githubauto/githubauto.sh
@@ -6,27 +6,24 @@ localPATH="/script"
 # remote repository of the script
 remoteRepo="https://github.com/ZeroneVu/shell.git"
 
+# determine hostname
+hostName=$(hostname)
+
+if [ ! `git -C $localPATH branch --list $hostName `]
+then
+   hostName="master"
+fi
+
 git -C $localPATH remote add upstream $remoteRepo
 git -C $localPATH fetch origin
-git -C $localPATH checkout master
+git -C $localPATH checkout $hostName
 
-newCommitID=$(git -C $localPATH log master..origin/master --oneline | awk {'print $1'})
+newCommitID=$(git -C $localPATH log $hostName..origin/$hostName --oneline | awk {'print $1'})
 
 if [[ ! -z $newCommitID ]]; then
-  echo "has something"
+  echo "has new commit for branch \""$hostName"\""
+  git -C $localPATH reset --hard origin/$hostName
+  supervisorctl restart minicheck
 else
   echo "nothing"
 fi
-
-
-#git -C $localPATH reset --hard origin/master
-
-sep='---------------'                                   
-for d in $localPATH/*; do
-  echo $sep"Processing" $d$sep
-  
-  # git -C $d remote add upstream $remoteRepo
-  # git -C $d fetch origin
-  #git -C $d log HEAD..origin/master --oneline
-  
-done
